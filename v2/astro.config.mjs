e5417dc588b025bd29be9486187a4bbb9f160066
Vibeke LeCocq <hello@vibekestolan.com>
2025-01-02 14:08:37 -0800

    Removed unused dependency

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
-import fulldev from 'fulldev-ui/integration'
 
 export default defineConfig({
   site: "https://vibekestolan.com/",
-  integrations: [
-    fulldev()
-  ]
+  integrations: []
 });
